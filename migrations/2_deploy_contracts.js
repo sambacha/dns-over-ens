5b7ece37436d817c6a04d3dc814d0a0015d5465c
JonahGroendal <jone951@gmail.com>
2019-07-13 18:20:20 -0400

    Created bash script to update dependant contracts' addresses

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,13 @@
-const DNSRegistrar = artifacts.require('DNSRegistrar');
-const ENSRegistry = artifacts.require('@ensdomains/ens/ENSRegistry');
-const FIFSRegistrar = artifacts.require('@ensdomains/ens/FIFSRegistrar');
+const namehash = require('eth-ens-namehash');
+const fs = require('fs');
+const forge = require('node-forge');
 const X509ForestOfTrustArtifact = require('x509-forest-of-trust/build/contracts/X509ForestOfTrust.json');
 const ENSRegistryArtifact = require('@ensdomains/ens/build/contracts/ENSRegistry.json');
 const FIFSRegistrarArtifact = require('@ensdomains/ens/build/contracts/FIFSRegistrar.json');
-const namehash = require('eth-ens-namehash');
-const fs = require('fs')
-const forge = require('node-forge')
+const DNSRegistrar = artifacts.require('DNSRegistrar');
+const ENSRegistry = artifacts.require('ENSRegistry');
+const FIFSRegistrar = artifacts.require('FIFSRegistrar');
+const X509ForestOfTrust = artifacts.require('X509ForestOfTrust');
 
 module.exports = function(deployer, network, accounts) {
   if (network === 'kovan') {
@@ -21,9 +22,11 @@ module.exports = function(deployer, network, accounts) {
     )
     deployer.then(() => {
       const pemCert = fs.readFileSync(__dirname + '/FakeLERootX1.pem');
+      const certBytes = '0x' + forge.asn1.toDer(forge.pki.certificateToAsn1(forge.pki.certificateFromPem(pemCert))).toHex()
       const pubKeyBytes = '0x' + forge.asn1.toDer(forge.pki.publicKeyToAsn1(forge.pki.certificateFromPem(pemCert).publicKey)).toHex();
       const certId = web3.utils.sha3(pubKeyBytes);
       // Set DNSRegistrar as owner of 'dnsroot.test'
+      console.log('setting contract as owner of root node...')
       return FIFSRegistrar.at(FIFSRegistrarArtifact.networks[42].address)
       .then(testRegistrar => testRegistrar.register(web3.utils.sha3('dnsroot'), DNSRegistrar.address))
       // .then(testRegistrar => {
@@ -44,6 +47,17 @@ module.exports = function(deployer, network, accounts) {
       //   .then(() => testRegistrar.register(web3.utils.sha3('dnsroot'), DNSRegistrar.address))
       // })
       // Add LetsEncrypt's staging environment root certificate as a trust anchor
+      .then(() => X509ForestOfTrust.at(X509ForestOfTrustArtifact.networks[42].address))
+      .then(x509 => {
+        return x509.validNotAfter(web3.utils.sha3(pubKeyBytes))
+        .then(validNotAfter => {
+          if (parseInt(validNotAfter) === 0) {
+            console.log('adding root certificate...');
+            return x509.addCert(certBytes, pubKeyBytes);
+          }
+        })
+      })
+      .then(() => console.log('making root certificate a trust anchor...'))
       .then(() => DNSRegistrar.deployed())
       .then(instance => instance.addTrustAnchor(certId))
     })
---
+++ updateDeps.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cp ../x509-forest-of-trust/build/contracts/X509ForestOfTrust.json node_modules/x509-forest-of-trust/build/contracts/X509ForestOfTrust.json
+cp ../ens/build/contracts/ENSRegistry.json node_modules/@ensdomains/ens/build/contracts/ENSRegistry.json
+cp ../ens/build/contracts/FIFSRegistrar.json node_modules/@ensdomains/ens/build/contracts/FIFSRegistrar.json
