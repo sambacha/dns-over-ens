6c5bd3e8d02cff3eb2ed4d84cbfa86fdfdd7f65a
JonahGroendal <jone951@gmail.com>
2019-09-03 03:26:38 -0400

    Changed package name. Redeployed to Kovan

--- updateDeps.sh
+++ updateDeps.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-cp ../x509-forest-of-trust/build/contracts/X509ForestOfTrust.json node_modules/x509-forest-of-trust/build/contracts/X509ForestOfTrust.json
-cp ../ens/build/contracts/ENSRegistry.json node_modules/@ensdomains/ens/build/contracts/ENSRegistry.json
-cp ../ens/build/contracts/FIFSRegistrar.json node_modules/@ensdomains/ens/build/contracts/FIFSRegistrar.json
+# cp ../x509-forest-of-trust/build/contracts/X509ForestOfTrust.json node_modules/x509-forest-of-trust/build/contracts/X509ForestOfTrust.json
+cp -r ../x509-forest-of-trust/build node_modules/x509-forest-of-trust/
+# cp ../ens/build/contracts/ENSRegistry.json node_modules/@ensdomains/ens/build/contracts/ENSRegistry.json
+# cp ../ens/build/contracts/FIFSRegistrar.json node_modules/@ensdomains/ens/build/contracts/FIFSRegistrar.json
+cp -r ../ens/build node_modules/@ensdomains/ens/
